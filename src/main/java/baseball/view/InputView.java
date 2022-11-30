ea4f85ab3647b50d09aac69cccb3a25ee586993f
woosung1223 <dntjd991223@naver.com>
2022-11-30 21:32:38 +0900

    feat(InputView): 입력 시 검증 기능 적용

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,5 +1,7 @@
 package baseball.view;
 
+import baseball.verifier.UserControlVerifier;
+import baseball.verifier.UserNumberVerifier;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
@@ -8,13 +10,15 @@ public class InputView {
 
     public static int readNumber() {
         String line = Console.readLine();
-        // TODO: 검증 절차 추가
+        UserNumberVerifier userNumberVerifier = new UserNumberVerifier();
+        userNumberVerifier.check(line);
         return Integer.parseInt(line);
     }
 
     public static int readControl() {
         String line = Console.readLine();
-        // TODO: 검증 절차 추가
+        UserControlVerifier userControlVerifier = new UserControlVerifier();
+        userControlVerifier.check(line);
         // TODO: int 자료형이 아닌 객체 타입으로 반환하기. Control 값은 언제나 바뀔 수 있다.
         return Integer.parseInt(line);
     }
