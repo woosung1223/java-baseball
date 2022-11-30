82516de5c70aeb1b8cb2d217954a2ac5963976c7
woosung1223 <dntjd991223@naver.com>
2022-11-30 22:24:30 +0900

    feat(BaseballGame): 게임 결과를 만들어내는 기능 생성

--- src/main/java/baseball/domain/BaseballGame.java
+++ src/main/java/baseball/domain/BaseballGame.java
@@ -1,5 +1,9 @@
 package baseball.domain;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class BaseballGame {
     private BaseballNumber answer;
     private final int GAME_DIGIT = 3;
@@ -7,5 +11,24 @@ public class BaseballGame {
     public void initAnswer() {
         RandomNumGenerator randomNumGenerator = new RandomNumGenerator();
         answer = new BaseballNumber(randomNumGenerator.makeDistinctRandomNDigitNumber(GAME_DIGIT));
+    }
+
+    // TODO: DTO 를 반환하도록 재구성한다.
+    public List<Integer> getGameResult(int userInput) {
+        BaseballNumber userNumber = new BaseballNumber(makeIntegerToList(userInput));
+        AnswerComparator answerComparator = new AnswerComparator();
+
+        List<Integer> gameResult = answerComparator.getScore(userNumber, answer);
+        return gameResult;
+    }
+
+    private List<Integer> makeIntegerToList(int target) {
+        List<Integer> result = new ArrayList<>();
+        while (target > 0) {
+            result.add(target / 10);
+            target /= 10;
+        }
+        Collections.reverse(result);
+        return result;
     }
 }
