62e591f501ce2297da86f25988773dd1375c948b
woosung1223 <dntjd991223@naver.com>
2022-11-30 21:48:31 +0900

    feat(BaseballNumber): 유저 및 정답 번호를 저장하는 기능 생성

---
+++ src/main/java/baseball/domain/BaseballNumber.java
@@ -0,0 +1,11 @@
+package baseball.domain;
+
+import java.util.List;
+
+public class BaseballNumber {
+    private List<Integer> baseballNumber;
+
+    public BaseballNumber(List<Integer> baseballNumber) {
+        this.baseballNumber = baseballNumber;
+    }
+}
