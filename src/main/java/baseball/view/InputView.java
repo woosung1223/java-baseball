d70cde895e785993cf0cc445c31c937054a90ecb
woosung1223 <dntjd991223@naver.com>
2022-11-30 22:28:27 +0900

    refactor(InputView): 비 static화, 입력 메세지가 출력되도록 수정

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -5,17 +5,22 @@ import baseball.verifier.UserNumberVerifier;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+    private final String READ_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
+    private final String READ_CONTROL_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
     private InputView() {
     }
 
-    public static int readNumber() {
+    public int readNumber() {
+        System.out.print(READ_NUMBER_MESSAGE);
         String line = Console.readLine();
         UserNumberVerifier userNumberVerifier = new UserNumberVerifier();
         userNumberVerifier.check(line);
         return Integer.parseInt(line);
     }
 
-    public static int readControl() {
+    public int readControl() {
+        System.out.println(READ_CONTROL_MESSAGE);
         String line = Console.readLine();
         UserControlVerifier userControlVerifier = new UserControlVerifier();
         userControlVerifier.check(line);
