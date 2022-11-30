c605eed10ea65c7b3b42c2e0776faaf413c683b9
woosung1223 <dntjd991223@naver.com>
2022-11-30 21:53:09 +0900

    refactor(BaseballGame): 메소드명이 의미를 더 잘 나타내도록 변경

--- src/main/java/baseball/domain/BaseballGame.java
+++ src/main/java/baseball/domain/BaseballGame.java
@@ -4,7 +4,7 @@ public class BaseballGame {
     private BaseballNumber answer;
     private final int GAME_DIGIT = 3;
 
-    public void initializeGame() {
+    public void initAnswer() {
         RandomNumGenerator randomNumGenerator = new RandomNumGenerator();
         answer = new BaseballNumber(randomNumGenerator.makeDistinctRandomNDigitNumber(GAME_DIGIT));
     }
