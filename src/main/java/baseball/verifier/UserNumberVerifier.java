554446ac38c8842bf2d8239c3a9e23c8e074ce9a
woosung1223 <dntjd991223@naver.com>
2022-12-01 10:34:51 +0900

    refactor(UserNumberVerifier): 각각의 숫자가 범위에 부합하는지 체크

--- src/main/java/baseball/verifier/UserNumberVerifier.java
+++ src/main/java/baseball/verifier/UserNumberVerifier.java
@@ -8,11 +8,13 @@ public class UserNumberVerifier {
     private final String IS_NOT_NUMERIC = "[ERROR] 입력된 값은 숫자가 아닙니다.";
     private final String IS_OUT_OF_RANGE = "[ERROR] " + NUMBER_SIZE + "자리 수를 입력해주세요.";
     private final String EACH_IS_NOT_DISTINCT = "[ERROR] 입력된 값에서 중복된 자리수가 존재합니다.";
+    private final String EACH_OUT_OF_RANGE = "[ERROR] 입력된 값은 범위에서 벗어납니다.";
 
     public void check(String target) {
         checkNumeric(target);
         checkNumberRange(target);
         checkEachDistinct(target);
+        checkEachInRange(target);
     }
 
     private void checkNumeric(String target) {
@@ -37,6 +39,17 @@ public class UserNumberVerifier {
 
         if (distinctValueCount != NUMBER_SIZE) {
             throw new IllegalArgumentException(EACH_IS_NOT_DISTINCT);
+        }
+    }
+
+    private void checkEachInRange(String target) {
+        int validNumberCount = (int) Stream.iterate(0, i -> i + 1)
+                .limit(target.length())
+                .filter(i -> target.charAt(i) >= '1' && target.charAt(i) <= '9')
+                .count();
+
+        if (validNumberCount != target.length()) {
+            throw new IllegalArgumentException(EACH_OUT_OF_RANGE);
         }
     }
 }
